ddff62642e2f2155164e66edfdafb0dc644a229f
Matthieu C. <886074+teto@users.noreply.github.com>
2025-06-24 12:31:29 +0200

    added tests for is_japanese

--- spec/tokenizer_spec.lua
+++ spec/tokenizer_spec.lua
@@ -1,21 +1,27 @@
--- This is a placeholder for tokenizer_spec tests.
--- You can add your tests here to validate the functionality of your tokenizer module.
-
--- Import the module containing the is_katakana function
+-- Import the module containing the is_japanese function
 local utils = require('jap-nvim.utils')
 
--- A simple test suite for the is_katakana function
-
-describe("is_katakana function", function()
-    it("should return true for a Katakana character", function()
-        assert.is_true(utils.is_katakana(vim.fn.char2nr("カ")))
+describe("is_japanese function", function()
+    it("should return true for a Hiragana character", function()
+        assert.is_true(utils.is_japanese("あ"))
     end)
 
-    it("should return false for a non-Katakana character", function()
-        assert.is_false(utils.is_katakana(vim.fn.char2nr("あ")))  -- Hiragana
-        assert.is_false(utils.is_katakana(vim.fn.char2nr("あ")))  -- Hiragana
-        assert.is_false(utils.is_katakana(vim.fn.char2nr("漢")))  -- Kanji
-        assert.is_false(utils.is_katakana(vim.fn.char2nr("A")))  -- Latin
+    it("should return true for a Katakana character", function()
+        assert.is_true(utils.is_japanese("カ"))
+    end)
+
+    it("should return true for a Common Kanji character", function()
+        assert.is_true(utils.is_japanese("漢"))
+    end)
+
+    it("should return true for a Half-width Katakana character", function()
+        assert.is_true(utils.is_japanese("ｶ"))
+    end)
+
+    it("should return false for a non-Japanese character", function()
+        assert.is_false(utils.is_japanese("A"))  -- Latin
+        assert.is_false(utils.is_japanese("3"))  -- Digit
+        assert.is_false(utils.is_japanese("@"))  -- Special character
     end)
 end)
 
