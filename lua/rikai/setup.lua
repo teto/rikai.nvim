21cd598253c0e3d31b68526a5c4fd2c6ae8cbebb
teto <886074+teto@users.noreply.github.com>
2026-01-27 02:30:06 +0100

    fix(live_hl): it seems to now work much better
    it is largely wasteful as we keep tokenizing the same line over and over.

--- lua/rikai/setup.lua
+++ lua/rikai/setup.lua
@@ -3,15 +3,17 @@
 
 local M = {}
 
-local highlighter = require("rikai.highlighter")
+-- local highlighter = require("rikai.highlighter")
 
+-- disabled temporarily, should be replaced by live_hl module eventually
 ---@return nil
 function M.enable_on_cursor()
-	vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-		group = highlighter.highlight_group,
-		pattern = "*",
-		callback = highlighter.highlight_current_word,
-	})
+	-- vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+	-- 	group = highlighter.highlight_group,
+	--        -- TODO replace by buffer
+	-- 	pattern = "*",
+	-- 	callback = highlighter.highlight_current_token,
+	-- })
 end
 
 return M
