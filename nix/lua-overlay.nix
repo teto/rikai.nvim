e8a0b14a079d434f195f8a6f6b9fa0cafd552a78
teto <886074+teto@users.noreply.github.com>
2026-09-16 22:22:15 +0200

    build: fixed rockspec
    dependencies had not been updated properly. We should generate it from
    lux

--- nix/lua-overlay.nix
+++ nix/lua-overlay.nix
@@ -8,26 +8,21 @@ final: prev: {
         lua,
         mega-cmdparse,
         mega-logging,
-        sqlite,
+        lsqlite3,
         luautf8,
       }:
       buildLuarocksPackage {
         pname = "rikai.nvim";
         version = "0.1.0-1";
-        knownRockspec = pkgs.runCommand "rikai.nvim-0.1.0-1.rockspec" { } ''
-          substitute ${../rikai.nvim-0.1.0-1.rockspec} $out \
-            --replace-fail '"lsqlite3",' '"sqlite",'
-        '';
-        # clean
+        knownRockspec = ../rikai.nvim-0.1.0-1.rockspec;
         src = pkgs.lib.cleanSource ../.;
 
         disabled = lua.luaversion != "5.1";
         propagatedBuildInputs = [
           mega-cmdparse
           mega-logging
-          sqlite
-          luautf8 # installed by nx
-
+          lsqlite3
+          luautf8
         ];
 
         runtimeDeps = [
@@ -37,7 +32,8 @@ final: prev: {
         meta = {
           homepage = "https://github.com/teto/rikai.nvim";
           description = "rikaitan for neovim, i.e., japanese translation integrated ";
-          license.fullName = "LGPL-3.0";
+          license = pkgs.lib.licenses.gpl3Only;
+
         };
       }
     ))
