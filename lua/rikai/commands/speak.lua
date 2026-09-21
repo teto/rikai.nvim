6df709c1b7bd88b486b63174257135a46a62778b
teto <886074+teto@users.noreply.github.com>
2026-09-21 04:05:02 +0200

    chore: have "Rikai speak" mention it's WIP

---
+++ lua/rikai/commands/speak.lua
@@ -0,0 +1,7 @@
+local fn = function ()
+    -- python3 -m kokoro --text "The sky above the port was the color of television, tuned to a dead channel." -o file.wav --debug
+
+    print("speak is not implemented yet")
+end
+
+return fn
